eed0885ea4c9d0ca81589d82d7be7061046f4ea8
LeoBai-0 <leobaijunye0203@gmail.com>
2024-12-10 23:26:42 -0500

    a

--- mern/server/server.js
+++ mern/server/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/record", records);
-app.use("meal", meals);
+app.use("/meal", meals);
 
 // start the Express server
 app.listen(PORT, () => {
