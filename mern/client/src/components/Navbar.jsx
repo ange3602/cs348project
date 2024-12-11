9660b28f66eca6263ea0dda1ad0a0d1e2086910b
LeoBai-0 <leobaijunye0203@gmail.com>
2024-12-10 22:58:32 -0500

    1 tiny change

--- mern/client/src/components/Navbar.jsx
+++ mern/client/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ export default function Navbar() {
 
           <NavLink
             className="inline-flex items-center ml-4 justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
-            to="/food-list"
+            to="/meals"
           >
             Meals
           </NavLink>
